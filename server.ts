5915aeec43b267c27008789e2556a81e63938424
amanpratap90 <amanpratap9076@gmail.com>
2026-01-20 02:58:00 +0530

    Add new allowed origin for CORS

--- server.ts
+++ server.ts
@@ -16,6 +16,8 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
     'http://localhost:5173',
+    'http://localhost:3000',
+    
     'https://status-frontend-q9a3.vercel.app'
 ].filter(Boolean);
 
